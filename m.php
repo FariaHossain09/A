25f9f0ddda51df5a129e35685609ec8932f7c17a
Faria2000 <fariahocane2000@gmail.com>
2022-01-24 19:06:14 +0600

    first commit

---
+++ m.php
@@ -0,0 +1,23 @@
+<!DOCTYP html>
+<html>
+<body>
+<h1 style="color:blue;text-align:center;">This is a heading</h1>
+<p style="color:red;">This is a paragraph.</p>
+
+<body>
+
+<div class="header">
+  <h1>Header</h1>
+</div>
+
+<div class="topnav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+
+</body>
+
+
+</body>
+</html>
